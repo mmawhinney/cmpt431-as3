2fa3e9b576f455ef2b5adde8e0c395b10b49b4b2
Matheson Mawhinney <mmawhinn@sfu.ca>
2016-11-05 18:36:49 -0700

    adding recursion

--- othello-good-ai.c
+++ othello-good-ai.c
@@ -4,15 +4,17 @@
 
 #include "othello.h"
 
-#define DEPTH 4
+#define DEPTH 1
 
 int GoodAITurn(Board *b, int colour) {
 	// Put your code for minimax here
-	int depth = 2;
+	// int depth = 2;
 	printf("Turn O\n");
 	printf("Pre AI: black: %llu, White %llu\n", b->disks[X_BLACK],
 			b->disks[O_WHITE]);
-	return minMax(b, 0, depth, colour);
+	int turnScore = minMax(b, 0, DEPTH, colour);
+	printf("Turn score: %d\n", turnScore);
+	return turnScore;//minMax(b, 0, DEPTH, colour);
 }
 
 ull doMove(Board *b, ull move, int colour) {
@@ -31,14 +33,16 @@ ull doMove(Board *b, ull move, int colour) {
 int minMax(Board *b, ull move, int depth, int colour) {
 	Board legal_moves;
 	int num_moves = EnumerateLegalMoves(*b, colour, &legal_moves);
+	int toUse = 0;
 	// TODO: terminal node
-	printf("moves: %d\n", num_moves);
-	if (depth <= 0) {
-		return costHeur(move);
+	printf("moves: %d, depth: %d\n", num_moves, depth);
+	if (depth == 0 || num_moves == 0) {
+		
+		return CountBitsOnBoard(b, colour);
 	}
-	if (num_moves > 0) {
+	// if (num_moves > 0) {
 		Board potentialBoards[num_moves];
-		int toUse = 0;
+		// int toUse = 0;
 		ull best;
 		// max
 		if (colour) {
@@ -48,13 +52,14 @@ int minMax(Board *b, ull move, int depth, int colour) {
 			for (int i = 0; i < num_moves; i++) {
 				potentialBoards[i] = *b;
 				moves = doMove(&potentialBoards[i], moves, colour);
-				int count = CountBitsOnBoard(&potentialBoards[i], colour);
-
-				if (best < count) {
-					best = count;
+				// int count = CountBitsOnBoard(&potentialBoards[i], colour);
+				int recurse = minMax(&potentialBoards[i], move, depth - 1, colour);
+				if (recurse > best) {
+					best = recurse;
 					toUse = i;
 				}
 			}
+			return best;
 			// min
 		} else {
 			best = ULLONG_MAX;
@@ -63,25 +68,25 @@ int minMax(Board *b, ull move, int depth, int colour) {
 			for (int i = 0; i < num_moves; i++) {
 				potentialBoards[i] = *b;
 				moves = doMove(&potentialBoards[i], moves, !colour);
-				int count = CountBitsOnBoard(&potentialBoards[i], !colour);
-
-				if (best > count) {
-					best = count;
+				// int count = CountBitsOnBoard(&potentialBoards[i], !colour);
+				int recurse = minMax(&potentialBoards[i], move, depth - 1, colour);
+				if (recurse < best) {
+					best = recurse;
 					toUse = i;
 				}
 			}
-
+			return best;
 		}
+		
 		printf("%d\n", toUse);
 		*b = potentialBoards[toUse];
 		PrintBoard(*b);
 		printf("Post AI: black: %llu, White %llu\n", b->disks[X_BLACK],
-				b->disks[O_WHITE]);
-
-		return best;
-	} else {
-		return 0;
-	}
+		b->disks[O_WHITE]);
+		// return best;
+	// } else {
+		// return 0;
+	// }
 }
 
 int costHeur(ull move) {
