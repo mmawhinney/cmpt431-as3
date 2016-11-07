d9607662d8171a67f0484c5ee44158590431c754
atlee <atlee@sfu.ca>
2016-11-06 20:13:01 -0800

    1 error, 186

--- othello-good-ai2.c
+++ othello-good-ai2.c
@@ -12,7 +12,16 @@ int minMax(Board *b, int depth, int colour);
 ull doMove(Board *b, ull move, int colour);
 
 int GoodAITurn(Board *b, int colour) {
-	return minMax(b, DEPTH, colour);
+	Board legal_moves;
+	Board toUse = *b;
+	int num_moves = EnumerateLegalMoves(toUse, colour, &legal_moves);
+	if(num_moves > 0) {
+		cilk_spawn minMax(b, DEPTH, colour);
+		cilk_sync;
+	} else {
+		return 0;
+	}
+	return 1;
 	//minMax(b, 0, DEPTH, colour);
 }
 
@@ -28,25 +37,29 @@ ull doMove(Board *b, ull move, int colour) {
 int minMax(Board *b, int depth, int colour) {
 	Board legal_moves;
 	int num_moves = EnumerateLegalMoves(*b, colour, &legal_moves);
-	if (depth == DEPTH && num_moves == 0) {
+	int terminal = num_moves == 0;
+	int end = depth == DEPTH;
+	int otherEnd = depth == 0;
+	if (end && terminal) {
 		return 0;
-	} else if (depth == 0 || num_moves == 0) {
+	} else if (otherEnd || terminal) {
 		return CountBitsOnBoard(b, colour);
 	}
 	Board potentialBoards[num_moves];
 	ull moves = legal_moves.disks[colour];
-	printf("%p\n", &moves);	
 	for(int i = 0; i < num_moves; i++) {
 		potentialBoards[i] = *b;
 		moves = doMove(&potentialBoards[i], moves, colour);
 	}
-	CILK_C_REDUCER_MAX_INDEX(maxMove, int, 0);
-	CILK_C_REDUCER_MIN_INDEX(minMove, int, 0);
+	int toUse = 0;
 	cilk_for (int i = 0; i < num_moves; i++) {
 		Board *thisMove = &potentialBoards[i];
+		CILK_C_REDUCER_MIN_INDEX(minMove, int, 0);
+		CILK_C_REDUCER_MAX_INDEX(maxMove, int, 0);
 		if (colour) {
 			CILK_C_REGISTER_REDUCER(maxMove);
 			CILK_C_REDUCER_MAX_INDEX_CALC(maxMove, i, minMax(thisMove, depth - 1, !colour));
+			toUse = REDUCER_VIEW(maxMove).index;
 			CILK_C_UNREGISTER_REDUCER(maxMove);
 		} else {
 			CILK_C_REGISTER_REDUCER(minMove);
@@ -55,8 +68,7 @@ int minMax(Board *b, int depth, int colour) {
 		}
 	}
 	if(colour){
-		int index = REDUCER_VIEW(maxMove).index;
-		*b = potentialBoards[index];
+		*b = potentialBoards[toUse];
 		return CountBitsOnBoard(b, colour);	
 	} else {
 		return CountBitsOnBoard(b, !colour);
